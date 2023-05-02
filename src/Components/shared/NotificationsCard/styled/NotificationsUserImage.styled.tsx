33ee35944e15c214a3aee9aa89f30fa83e9fa157
Sajjad-iq <rasooool59@gmail.com>
2023-05-02 19:18:47 +0300

    css issues

--- src/Components/shared/NotificationsCard/styled/NotificationsUserImage.styled.tsx
+++ src/Components/shared/NotificationsCard/styled/NotificationsUserImage.styled.tsx
@@ -2,21 +2,21 @@ import styled from "styled-components";
 import { device } from "../../../../assets/breackPoints";
 
 export const NotificationsUserImage = styled.img`
-  width: 42px;
-  height: 42px;
+  width: 47px;
+  height: 47px;
   margin-right:10px;
   border-radius: 50%;
   object-fit: cover;
 
 
 @media ${device.tablet} {
-    width: 55px;
-    height: 55px;
+    width: 50px;
+    height: 50px;
 
   }
   @media ${device.laptop} {
-    width: 45px;
-    height: 45px;
+    width: 55px;
+    height: 55px;
 
   }
 `
