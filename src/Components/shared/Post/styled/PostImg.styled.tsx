f203e0443e4af2ce6ea7ca9c0f59021d1725441c
Sajjad-iq <rasooool59@gmail.com>
2023-05-31 08:09:39 +0300

    performance

--- src/Components/shared/Post/styled/PostImg.styled.tsx
+++ src/Components/shared/Post/styled/PostImg.styled.tsx
@@ -1,7 +1,22 @@
 import styled from "styled-components";
+import { device } from "../../../../assets/breackPoints";
+import { Colors } from "../../../../assets/Colors";
 
 export const PostImg = styled.img`
  width: 100%;
- height: 200px;
+ height: 230px;
+ object-fit:scale-down;
+ background-color: ${Colors.Primary.SoftBlack};
 
+ @media ${device.tablet} {
+    height: 450px;
+}
+
+   @media ${device.laptop} {
+    height: 250px;
+  }
+
+  @media ${device.laptopL} {
+    height: 275px;
+  }
 `
