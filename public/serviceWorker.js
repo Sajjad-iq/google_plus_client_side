b207ab28dd80b5f1dee3e24f42f834cff9613bfe
Sajjad-iq <rasooool59@gmail.com>
2023-06-02 14:33:02 +0300

    worker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -7,7 +7,7 @@ const self = this;
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => {
+            .then(async (cache) => {
                 console.log('Opened cache');
 
                 return cache.addAll(urlsToCache);
@@ -19,7 +19,7 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then(() => {
+            .then(async () => {
                 return fetch(event.request)
                     .catch(() => caches.match('offline.html'))
             })
@@ -32,8 +32,8 @@ self.addEventListener('activate', (event) => {
     cacheWhitelist.push(CACHE_NAME);
 
     event.waitUntil(
-        caches.keys().then((cacheNames) => Promise.all(
-            cacheNames.map((cacheName) => {
+        caches.keys().then(async (cacheNames) => Promise.all(
+            cacheNames.map(async (cacheName) => {
                 if (!cacheWhitelist.includes(cacheName)) {
                     return caches.delete(cacheName);
                 }
