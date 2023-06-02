e53e63c4ddafda9e23c3a13038b0a2db0b241e59
Sajjad-iq <rasooool59@gmail.com>
2023-06-02 17:06:25 +0300

    service worker test

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,37 @@ import react from "@vitejs/plugin-react";
 
 // https://vite-pwa-org.netlify.app/guide/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [react(), VitePWA({
+    registerType: "autoUpdate",
+    manifest: {
+      icons: [
+        {
+          "src": "maskable_icon.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "any maskable"
+        },
+        {
+          "src": "logo192.png",
+          "type": "image/png",
+          "sizes": "192x192"
+        },
+        {
+          "src": "logo256.png",
+          "type": "image/png",
+          "sizes": "256x256"
+        },
+        {
+          "src": "logo384.png",
+          "type": "image/png",
+          "sizes": "384x384"
+        },
+        {
+          "src": "logo512.png",
+          "type": "image/png",
+          "sizes": "512x512"
+        }
+      ]
+    }
+  })],
 });
