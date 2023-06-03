6337b527423dd4ccdbeb152ae9de59947febf6b2
Sajjad-iq <rasooool59@gmail.com>
2023-06-03 19:10:30 +0300

    posts image size fix

--- src/Components/shared/Post/styled/PostImg.styled.tsx
+++ src/Components/shared/Post/styled/PostImg.styled.tsx
@@ -4,19 +4,16 @@ import { Colors } from "../../../../assets/Colors";
 
 export const PostImg = styled.img`
  width: 100%;
- height: 230px;
+ max-height: 550px;
  object-fit:scale-down;
  background-color: ${Colors.Primary.SoftBlack};
 
  @media ${device.tablet} {
-    height: 450px;
 }
 
    @media ${device.laptop} {
-    height: 250px;
   }
 
   @media ${device.laptopL} {
-    height: 275px;
   }
 `
