e2aa4110b409779a22cc86d1be2b139c4d472077
Sajjad-iq <rasooool59@gmail.com>
2023-06-03 17:51:56 +0300

    sw updates handling

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,16 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './assets/GlobalStyles/index.css'
 import GlobalContextProvider from './Context/GlobalContext'
+import { registerSW } from "virtual:pwa-register";
 
+// add this to prompt for a refresh
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+});
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <GlobalContextProvider>
     <BrowserRouter>
