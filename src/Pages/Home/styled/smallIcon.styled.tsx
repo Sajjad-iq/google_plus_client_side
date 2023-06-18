12e06beb6b4a727f69c3300e50d354c68910aec9
Sajjad-iq <rasooool59@gmail.com>
2023-06-18 19:18:59 +0300

    red button issue

--- src/Pages/Home/styled/smallIcon.styled.tsx
+++ src/Pages/Home/styled/smallIcon.styled.tsx
@@ -1,8 +1,14 @@
 import styled from "styled-components";
+import { device } from "../../../assets/breackPoints";
 
 export const SmallIcon = styled.img`
   width: 75%;
   height: 25px;
   object-fit:cover;
 
+  @media ${device.desktop} {
+  height: 35px;
+
+}
 `
+
