622d004e0fcda114636e522d4bee30ea4c4c9d76
Sajjad-iq <rasooool59@gmail.com>
2023-09-06 13:16:31 +0300

    layouts issues

--- src/SplitScreen/index.tsx
+++ src/SplitScreen/index.tsx
@@ -1,9 +1,8 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { Aside } from '../Layout/Aside'
 import { FooterNav } from '../Layout/Footer'
 import { Nav } from '../Layout/Nav'
 import { ActiveToggle } from '../services/PostsServices/PostsScreenToggle'
-
 import { Footer } from './styled/footer'
 import { Header } from './styled/Header'
 import { LeftSide } from './styled/LeftSide'
@@ -15,6 +14,7 @@ import { SplitWrapper } from './styled/SplitWrapper'
 export const SplitScreen = () => {
 
     const { IsPageActive, Toggle, setIsPageActive } = ActiveToggle()
+    const Location = useLocation()
 
     return (
         <SplitWrapper >
@@ -23,7 +23,7 @@ export const SplitScreen = () => {
                 <Nav MenuButtonHandler={Toggle} isMenuButtonActive={IsPageActive} />
             </Header>
 
-            <Row>
+            <Row style={{ height: Location.pathname === "/Collections" || Location.pathname === "/Communities" ? "calc(100% - 114px)" : "calc(100% - 66px)" }}>
                 <LeftSide>
                     <Aside setIsPageActive={setIsPageActive} IsPageActive={IsPageActive} />
                 </LeftSide>
